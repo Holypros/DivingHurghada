36708db50fed8bcce47981a9601bec27f8fdb9fd
RehabAlaa <habiba.alaa97@gmail.com>
2022-05-12 12:07:32 +0200

    Edit equations in depth script

--- Assets/Scripts/DepthCalc.cs
+++ Assets/Scripts/DepthCalc.cs
@@ -8,22 +8,23 @@ public class DepthCalc : MonoBehaviour
 {
     [SerializeField] Image bar;
     [SerializeField] TextMeshProUGUI depthTxt;
-    [SerializeField] float maxY = 6.8f;
-    [SerializeField] float minY = -20f;
-    [SerializeField] float playerLength = 2f;
+    [SerializeField] float maxY;
+    [SerializeField] float minY;
+    [SerializeField] float playerLength;
 
     Transform playerTransform;
-
+    float size;
     // Start is called before the first frame update
     void Start()
     {
         playerTransform = transform;
+        size = maxY - minY;
     }
 
     // Update is called once per frame
     void Update()
     {
-        bar.fillAmount = (playerTransform.position.y - playerLength) / (maxY - minY);
-        depthTxt.text = (int) ((maxY - minY) - playerTransform.position.y) + " m";
+        bar.fillAmount = (size - (maxY - playerTransform.position.y)) / size;
+        depthTxt.text = (int) (maxY - playerTransform.position.y) + " m";
     }
 }
