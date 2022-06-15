b3a3a59ef6382a7b487b46839adc4fed8c95879e
SherifaSayed <sherifasayedabrahem@gmail.com>
2022-06-15 14:13:38 +0200

    small change

--- Assets/_FreeLookCamera3rdPersonCharacterController/TriviaFolder/TriviaScript.cs
+++ Assets/_FreeLookCamera3rdPersonCharacterController/TriviaFolder/TriviaScript.cs
@@ -51,6 +51,7 @@ public class TriviaScript : MonoBehaviour
     private void OnTriggerExit(Collider other)
     {
         UiManager.UiInstance.TriviaButton.gameObject.SetActive(false);
+        UiManager.UiInstance.Triviapanel.gameObject.SetActive(false);
     }
 
     public void triviabutton()
