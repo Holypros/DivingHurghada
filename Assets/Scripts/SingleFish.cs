282e8c66696812c7836360eae54062b3d3086755
SherifaSayed <sherifasayedabrahem@gmail.com>
2022-06-19 11:27:34 +0200

    fish changes

--- Assets/Scripts/SingleFish.cs
+++ Assets/Scripts/SingleFish.cs
@@ -7,10 +7,13 @@ public class SingleFish : MonoBehaviour
     public Transform[] points;
     int n = 0;
     float speed = 3f;
+    Quaternion rot;
+    Vector3 dir;
 
     private void Update()
     {
         Transform p = points[n];
+       
         if (Vector3.Distance(transform.position, p.position) < 0.01f)
         {
             n = (n + 1) % points.Length;
@@ -18,6 +21,9 @@ public class SingleFish : MonoBehaviour
         else
         {
             transform.position = Vector3.MoveTowards(transform.position, p.position, speed * Time.deltaTime);
+            //dir = (points[n].position - points[n-1].position).normalized;
+            //rot = Quaternion.LookRotation(dir);
+            //transform.rotation = Quaternion.Slerp(transform.rotation, rot, 0.3f);
             transform.LookAt(points[n]);
 
         }
